1a0655cc8d6c3d0e8e32857341b62fe11846a32d
abz <s.martinez92@live.com>
2018-10-11 15:48:46 -0400

    pls deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var path = require('path');
 
 app.use(bodyParser.json());
